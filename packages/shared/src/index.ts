b30d16f74a1d8d9e491b1013c3cee21d09da541b
httperry <priyadarshan.m@outlook.com>
2026-09-16 09:32:15 +0530

    define MaskRegion schema and interval bounds checking in shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,5 @@ export * from "./constants.js";
 export * from "./types.js";
 export * from "./clientInfo.js";
 export * from "./cuts.js";
+export * from "./masks.js";
 export * from "./clockOffset.js";
